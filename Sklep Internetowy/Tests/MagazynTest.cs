d9588d7e231385da6c880f87fb98c8e3a3520f90
KGoncii <kacpergoncerz.dev@gmail.com>
2024-12-20 13:30:13 +0100

    poprawa działania testów jednostkowych

--- Sklep Internetowy/Tests/MagazynTest.cs
+++ Sklep Internetowy/Tests/MagazynTest.cs
@@ -1,100 +1,96 @@
-﻿using System;
-using System.Collections.Generic;
-using Xunit;
+﻿using Xunit;
 using Sklep_Internetowy.Models;
 
 public class MagazynTests
 {
+    private Magazyn magazyn;
+    private List<Produkt> addedProducts;
+
+    public MagazynTests()
+    {
+        magazyn = new Magazyn();
+        addedProducts = new List<Produkt>();
+    }
+
     [Fact]
     public void DodajProdukt_ProduktNieIstnieje_ProduktDodany()
     {
         // Arrange
-        var magazyn = new Magazyn();
         var produkt = new Produkt("NowyProdukt", "Opis", 10, 50.0m, new string[] { "Kategoria" });
 
         // Act
         magazyn.DodajProdukt(produkt);
+        addedProducts.Add(produkt);
 
         // Assert
         Assert.Contains(produkt, magazyn.produkty.Values);
+        Dispose();
     }
 
     [Fact]
     public void DodajProdukt_ProduktIstnieje_WyswietlaKomunikat()
     {
         // Arrange
-        var magazyn = new Magazyn();
         var produkt = new Produkt("NowyProdukt", "Opis", 10, 50.0m, new string[] { "Kategoria" });
         magazyn.DodajProdukt(produkt);
+        addedProducts.Add(produkt);
 
         // Act
         var consoleOutput = CaptureConsoleOutput(() => magazyn.DodajProdukt(produkt));
 
         // Assert
         Assert.Contains("Produkt o ID", consoleOutput);
+        Dispose();
     }
 
     [Fact]
     public void UsunProdukt_ProduktIstnieje_ProduktUsuniety()
     {
         // Arrange
-        var magazyn = new Magazyn();
         var produkt = new Produkt("NowyProdukt", "Opis", 10, 50.0m, new string[] { "Kategoria" });
         magazyn.DodajProdukt(produkt);
+        addedProducts.Add(produkt);
 
         // Act
         magazyn.UsunProdukt(produkt.Id);
+        addedProducts.Remove(produkt);
 
         // Assert
         Assert.DoesNotContain(produkt, magazyn.produkty.Values);
+        Dispose();
     }
 
     [Fact]
     public void UsunProdukt_ProduktNieIstnieje_WyswietlaKomunikat()
     {
-        // Arrange
-        var magazyn = new Magazyn();
-
         // Act
         var consoleOutput = CaptureConsoleOutput(() => magazyn.UsunProdukt(999));
 
         // Assert
         Assert.Contains("Produkt o ID", consoleOutput);
-    }
-
-    [Fact]
-    public void WyswietlProdukty_BrakProduktow_WyswietlaKomunikat()
-    {
-        // Arrange
-        var magazyn = new Magazyn();
-
-        // Act
-        var consoleOutput = CaptureConsoleOutput(() => magazyn.WyswietlProdukty());
-
-        // Assert
-        Assert.Contains("Brak produktów w magazynie", consoleOutput);
+        Dispose();
     }
 
     [Fact]
     public void WyswietlProdukty_ProduktyIstnieja_WyswietlaProdukty()
     {
         // Arrange
-        var magazyn = new Magazyn();
         var produkt = new Produkt("NowyProdukt", "Opis", 10, 50.0m, new string[] { "Kategoria" });
         magazyn.DodajProdukt(produkt);
+        addedProducts.Add(produkt);
 
         // Act
         var consoleOutput = CaptureConsoleOutput(() => magazyn.WyswietlProdukty());
 
         // Assert
         Assert.Contains("ID: ", consoleOutput);
+        Dispose();
     }
 
     [Fact]
     public void WczytajProdukty_ListaProduktow_ProduktyDodane()
     {
         // Arrange
-        var magazyn = new Magazyn();
         var produkty = new List<Produkt>
         {
             new Produkt("Produkt1", "Opis1", 10, 99.99m, new string[] { "Kategoria1" }),
@@ -103,12 +99,14 @@ public class MagazynTests
 
         // Act
         magazyn.WczytajProdukty(produkty);
+        addedProducts.AddRange(produkty);
 
         // Assert
         foreach (var produkt in produkty)
         {
             Assert.Contains(produkt, magazyn.produkty.Values);
         }
+        Dispose();
     }
 
     private string CaptureConsoleOutput(Action action)
@@ -117,5 +115,13 @@ public class MagazynTests
         Console.SetOut(consoleOutput);
         action();
         return consoleOutput.ToString();
+    }
+    public void Dispose()
+    {
+        foreach (var produkt in addedProducts)
+        {
+            magazyn.UsunProdukt(produkt.Id);
+        }
+        addedProducts.Clear();
     }
 }
